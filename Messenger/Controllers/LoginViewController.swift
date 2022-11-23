79a39a6ca730d469802e7719738ab8a11f745211
tkoszhanov <tilek.koszhanov@mail.ru>
2022-11-23 22:08:44 +0500

    Initial commit

---
+++ Messenger/Controllers/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Messenger
+//
+//  Created by Tilek Koszhanov on 11/23/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
